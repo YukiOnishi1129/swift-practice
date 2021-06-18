18cb0aef6252648f6df042b0b27facf060743891
yuki <yuki.onishi1129@gmail.com>
2021-06-18 16:50:02 +0900

    update: text

--- MyFirstApp/MyFirstApp/ContentView.swift
+++ MyFirstApp/MyFirstApp/ContentView.swift
@@ -5,11 +5,14 @@
 //  Created by YUKI on 2021/06/18.
 //
 
+// import: フレームワークを読み込み
 import SwiftUI
 
+// View: 画面に配置される部品やレイアウト
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .font(.largeTitle)
             .padding()
     }
 }
