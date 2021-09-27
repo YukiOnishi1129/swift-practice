76a76944465191f345d788f5ce148ca52bf92c2e
yuki <yuki.onishi1129@gmail.com>
2021-09-27 17:04:43 +0900

    add: setup

---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by YUKI on 2021/09/27.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
