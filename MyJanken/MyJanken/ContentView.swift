4c1895468d0ad2c67693d44c08be6db90059051c
yuki <yuki.onishi1129@gmail.com>
2021-09-27 16:39:54 +0900

    add: repeat

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -53,8 +53,28 @@ struct ContentView: View {
             
             // 「じゃんけんをする」ボタン
             Button(action: {
-                // 次のジャンケンへ (乱数)
-                answerNumber = Int.random(in: 1...3)
+                
+                // 新しいジャンケンの結果を一時的に格納する変数を設ける
+                var newAnswerNumber = 0
+                
+                // ランダムに結果を出すが、前回の結果と異なる場合のみ採用
+                // repeatは繰り返しを意味する
+                repeat {
+                    // 1,2,3の変数をランダムに算出(乱数)
+                    newAnswerNumber = Int.random(in: 1...3)
+                    
+                    // 前回と同じ結果の時は、再度ランダムに数値を出す
+                    // 異なる結果の時は、repeatをにける
+                } while answerNumber == newAnswerNumber
+                
+                /**
+                    ループ処理
+                    repeact {
+                    } while 繰り返す条件
+                 */
+                
+                // 新しいジャンケンの結果を格納
+                answerNumber = newAnswerNumber
             }) {
                 // Buttonに表示させる文字の指定
                 Text("じゃんけんする！")
