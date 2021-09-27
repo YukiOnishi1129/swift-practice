db9187bd5d9fb3ed45e08b9c4d2ef45213fdac50
yuki <yuki.onishi1129@gmail.com>
2021-09-27 14:54:58 +0900

    add: create project

---
+++ MyJanken/MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by YUKI on 2021/09/27.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
