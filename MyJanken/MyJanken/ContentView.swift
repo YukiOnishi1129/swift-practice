b963b088715d20a919baf4dc9f2a88678502ed0e
yuki <yuki.onishi1129@gmail.com>
2021-09-27 16:28:03 +0900

    add: janken

--- MyJanken/MyJanken/ContentView.swift
+++ MyJanken/MyJanken/ContentView.swift
@@ -8,22 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 変数定義
+    // @Stateをつけることで、更新可能な変数を定義できる
+    // @Stateを付けずに変数定義すると、定数になる
+    @State var answerNumber: Int = 0
+    
     var body: some View {
         
         VStack {
-            // グー画像を指定
-            Image("gu")
-                // リサイズを指定
-                .resizable()
-                // 画面内に収まるように、アスペクト比(縦横比)を維持する指定
-                .aspectRatio(contentMode: .fit)
-            // ジャンケンの種類を指定
-            Text("グー")
+            
+            if answerNumber == 0 {
+                Text("これからじゃんけんをします")
+            } else if answerNumber == 1 {
+                // グー画像を指定
+                Image("gu")
+                    // リサイズを指定
+                    .resizable()
+                    // 画面内に収まるように、アスペクト比(縦横比)を維持する指定
+                    .aspectRatio(contentMode: .fit)
+                // ジャンケンの種類を指定
+                Text("グー")
+                
+            } else if answerNumber == 2 {
+                // チョキ画像を指定
+                Image("choki")
+                    // リサイズを指定
+                    .resizable()
+                    // 画面内に収まるように、アスペクト比(縦横比)を維持する指定
+                    .aspectRatio(contentMode: .fit)
+                // ジャンケンの種類を指定
+                Text("チョキ")
+                
+            } else {
+                // パー画像を指定
+                Image("pa")
+                    // リサイズを指定
+                    .resizable()
+                    // 画面内に収まるように、アスペクト比(縦横比)を維持する指定
+                    .aspectRatio(contentMode: .fit)
+                // ジャンケンの種類を指定
+                Text("パー")
+                
+            }
             
             // 「じゃんけんをする」ボタン
             Button(action: {
-                // Buttonがタプされた時の動き
-                print("タップされたよ！")
+                // 次のジャンケンへ
+                answerNumber = answerNumber + 1
             }) {
                 // Buttonに表示させる文字の指定
                 Text("じゃんけんする！")
