6ea3107689c20992a674ce95e6fcd19305d7beb0
yuki <yuki.onishi1129@gmail.com>
2021-09-28 16:53:17 +0900

    add: setup

---
+++ MyCamera/MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by YUKI on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
