5597432bf985f53c1812237c9ea67c3f9efd7018
yuki <yuki.onishi1129@gmail.com>
2021-09-28 17:44:23 +0900

    add: button

--- MyCamera/MyCamera/ContentView.swift
+++ MyCamera/MyCamera/ContentView.swift
@@ -9,8 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 縦方向にレイアウト
+        VStack {
+            // スペースを追加
+            Spacer()
+            // 「カメラを起動する」ボタン
+            Button(action: {
+                // ボタンをタップした時のアクション
+            }) {
+                // テキストを表示
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    // 文字列をセンタリングに指定
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
+        }
     }
 }
 
