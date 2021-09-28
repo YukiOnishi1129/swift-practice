b7a506f25889d8dc5a70d0b3937731a5ace1d1a6
yuki <yuki.onishi1129@gmail.com>
2021-09-28 12:40:02 +0900

    add: setup

---
+++ MyTimer/MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by YUKI on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
