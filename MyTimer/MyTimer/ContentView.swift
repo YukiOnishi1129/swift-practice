cf0f6cf1e32e952f86de45ee58dcb6af9062f768
yuki <yuki.onishi1129@gmail.com>
2021-09-28 13:04:47 +0900

    add: layouts

--- MyTimer/MyTimer/ContentView.swift
+++ MyTimer/MyTimer/ContentView.swift
@@ -10,11 +10,52 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Text("タイマー画面")
+            ZStack {
+                // 背景画像
+                Image("backgroundTimer")
+                    .resizable()
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                
+                //垂直方向にレイアウト
+//                View(部品)間の感覚を30にする
+                VStack(spacing: 30.0) {
+                    Text("残り10秒")
+                        // 文字サイズを指定
+                        .font(.largeTitle)
+                    
+                    // 水平方向にレイアウト
+                    HStack {
+                        // スタートボタン
+                        Button(action: {
+                            // ボタンをタップした時のアクション
+                        }) {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("startColor"))
+                                // 円形に切り抜く
+                                .clipShape(Circle())
+                        }
+                        
+                        // ストップボタン
+                        Button(action: {
+                            // ボタンをタップした時のアクション
+                        }) {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
             }
             // ナビゲーションバーにボタンを追加
-            .navigationBarItems(trailing:
+            // navigationBarItems: 画面上部のナビゲーションバー部分のレイアウト
+            .navigationBarItems(trailing: // trailing: 画面右端に寄せる
                 // ナビゲーション遷移
                 NavigationLink(
                     destination: SettingView()) {
