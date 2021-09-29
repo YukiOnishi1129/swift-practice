8c768395cd255169b8798d9f016a44192445b979
yuki <yuki.onishi1129@gmail.com>
2021-09-29 16:24:50 +0900

    add: date struct

--- MyOkashi/MyOkashi/ContentView.swift
+++ MyOkashi/MyOkashi/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    // OkashiDataw参照する状態関数
+    // ObservedObject: 複数データを外部ファイルと共有する場合に利用する
+    @ObservedObject var okashiDataList = OkashiData()
+    // 入力された文字列を保持する状態変数
+    @State var inputText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 垂直にレイアウト(横方向にレイアウト)
+        VStack {
+            // 文字を受け取るTextFieldを表示する
+            TextField("キーワードを入力してください",
+                    text: $inputText,
+                    onCommit: {
+                        // 入力完了直後に検索する
+                        okashiDataList.searchOkashi(keyword: inputText)
+                    }
+            )
+        }
     }
 }
 
--- MyOkashi/MyOkashi/Okashidata.swift
+++ MyOkashi/MyOkashi/Okashidata.swift
@@ -7,4 +7,38 @@
 
 import Foundation
 
-// お菓子データ
+// お菓子データ検索用クラス
+// ObservableObject: データ状態を管理するために使用
+class OkashiData: ObservableObject {
+    // JSONのデータ構造
+    // Codable: JSONのデータ項目名とプログラムの変数めいを同じ名前にすると、JSONを変換したときに、一括してデータを格納することができる
+    struct ResultJson: Codable {
+        // JSONのitem内のデータ構造
+        struct Item: Codable {
+            // お菓子の名称
+            let name: String?
+            // 掲載URL
+            let url: URL?
+            // 画像URL
+            let image: URL?
+        }
+        // 複数要素
+        let item: [Item]?
+    }
+    // Web API検索用メソッド 第一引数: keyword 検索したいワード
+    func searchOkashi(keyword: String) {
+        // デバッグエリアに出力
+        print(keyword)
+        
+        // お菓子の検索キーワードをURLエンコードする
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        // リクエストURLの組み立て
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+    }
+}
